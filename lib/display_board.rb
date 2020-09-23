f356303bccf0b679f99ebc667e0f9f48b715204e
Misel Evtimov <misel.evtimov@gmail.com>
2020-09-23 23:56:39 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,15 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+<<<<<<< HEAD
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+=======
+  puts "---------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "---------"
+  puts " #{board[8]} | #{board[7]} | #{board[8]} "
+>>>>>>> 891989e90d036f264cc1e50ca0273f0964d1f4fd
 end
